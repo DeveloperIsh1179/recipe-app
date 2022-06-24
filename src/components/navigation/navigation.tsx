e7f6f7c9e7aa29ee05797ff68939990a479bf215
DeveloperIsh1179 <ismael.sanchez.1179@gmail.com>
2022-06-24 00:06:17 -0700

    added navigation bar

---
+++ src/components/navigation/navigation.tsx
@@ -0,0 +1,18 @@
+import { ReactComponent as Chef } from 'assets/chef-svgrepo-com.svg';
+import './navigation.css';
+
+const Navigation = () => (
+  <div className="navigation-bar">
+    <div>
+      <Chef className="logo" />
+    </div>
+    <div>
+      <p>Hello</p>
+    </div>
+    <div>
+      <p>Hello</p>
+    </div>
+  </div>
+);
+
+export default Navigation;
